1d594662127716aadaebff3e7362ac575a77ebe8
Alan Orgaz Villegas <147757830+AlanOrgazVillegas@users.noreply.github.com>
2024-02-28 21:58:40 -0600

    Add files via upload
    Descripcion
    
    El metodo de la secante se puede pensar como una simplificacion del metodo de Newton-Raphson. En lugar de tomar la derivada de la funcion cuya raız se quiere encontrar, se aproxima por una recta secante (de ahı el nombre) a la curva, cuya pendiente es aproximadamente igual a la derivada en el punto inicial. 
    
    Algoritmo
    
    El método se basa en obtener la ecuación de la recta que pasa por los puntos (xn−1, f(xn−1)) y (xn, f(xn)). A dicha recta se le llama secante por cortar la gráfica de la función. En forma punto-pendiente, esta línea tiene la ecuación mostrada anteriormente. Posteriormente se escoge como siguiente elemento de la relación de recurrencia, xn+1, la intersección de la recta secante con el eje de abscisas obteniendo la fórmula, y un nuevo valor. Seguimos este proceso, hasta llegar a un nivel suficientemente alto de precisión (una diferencia lo suficientemente pequeñas entre xn y xn-1).
    
    Implementacion
    
    El método de la secante no se tiene en cuenta el signo de la función para estimar el siguiente punto. Se procede independientemente de los signos de la función. De todas maneras en algunos casos es más útil emplear el método de la secante. Este  método,  a  diferencia  del  de  bisección  y  Newton,  casi  nunca  falla  ya  que  solo  requiere  de  2 puntos al principio, y después el mismo método se va retroalimentando

---
+++ Ejemplos de metodo de secante/Metodo_secante_3/src/metodo_secante_3/Metodo_secante_3.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package metodo_secante_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alan
+ */
+public class Metodo_secante_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println(" EJEMPLO 3 METODO DE SECANTE ");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese Xi: ");
+        double x0 = scanner.nextDouble(); // Leer Xi "a"
+        System.out.print("Ingrese Xi-1: ");
+        double x1 = scanner.nextDouble(); // Leer Xi-1 "b"
+        System.out.print("Ingrese el número máximo de iteraciones: ");
+        int maxIter = scanner.nextInt(); // Leer el número máximo de iteraciones 
+        System.out.print("Ingrese el valor de tolerancia: ");
+        double tol = scanner.nextDouble(); // Leer el valor de tolerancia 
+
+        double root = metodoSecante(x0, x1, maxIter, tol); // Llamar al método metodoSecante 
+
+        System.out.printf("La raíz de la ecuación es: %.5f%n", root); // Imprimir el resultado del método metodoSecante con 5 dígitos de precisión después del punto decimal
+    }
+
+    public static double metodoSecante(double x0, double x1, int maxIter, double tol) {
+        if (func(x0) * func(x1) == 0) {
+            System.out.println("El método no puede encontrar una raíz."); // Verificar 
+            return -1;
+        }
+
+        double x2 = 0;
+        for (int i = 0; i < maxIter; i++) {
+            x2 = x1 - (func(x1) * (x1 - x0) / (func(x1) - func(x0))); // Calcular la siguiente aproximacion usando el método de la secante
+            if (Math.abs(x1 - x2) < tol) {
+                return x2; // Si el cambio en las aproximaciones es menor que el valor de tolerancia, devolver la aproximacion actual como la raíz
+            }
+            x0 = x1; // Actualizar el valor de x0
+            x1 = x2; // Actualizar el valor de x1
+        }
+        return x2; // Si se alcanza el número máximo de iteraciones sin encontrar una raíz con la tolerancia especificada, devolver la última aproximacion
+    }
+
+    public static double func(double x) {
+        return Math.pow(x, 2) - 5 * x + 6;
+    }
+    
+}
